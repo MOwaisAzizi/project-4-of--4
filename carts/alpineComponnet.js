3fc80faa01202042b0e3462db6e5cc0a0e3e506d
owias azizi <owaisazizi@jmail.com>
2024-01-11 03:19:08 +0430

    adding project#4

---
+++ carts/alpineComponnet.js
@@ -0,0 +1,18 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('toggleSidebar', () => ({
+        open: window.innerWidth <= 992 ? false : true,
+
+        toggle() {
+            this.open = !this.open
+        }
+    })),
+
+        Alpine.data('dropdown', () => ({
+            open: false,
+            toggle() {
+                this.open = !this.open
+            }
+        }))
+
+
+})
